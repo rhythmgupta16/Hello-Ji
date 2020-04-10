750dfd5bdc7d87eadcb97cc7ede6ab76dd0d418b
rhythmgupta16 <rhythmgupta1234@gmail.com>
2020-04-10 23:51:19 +0530

    Bottom navigation bar done

--- app/src/main/java/com/example/helloji/ProfileActivity.java
+++ app/src/main/java/com/example/helloji/ProfileActivity.java
@@ -1,14 +1,47 @@
 package com.example.helloji;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(TagsFragment.newInstance("", ""));
     }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_tags:
+                            openFragment(TagsFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_trending:
+                            openFragment(TrendingFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_explore:
+                            openFragment(ExploreFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_saved:
+                            openFragment(SavedFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
